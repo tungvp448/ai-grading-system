cfe2d4fce6aeb1df1c0e46410cc89dc95b722623
tungvp448 <tungvp.2003@gmail.com>
2026-08-19 15:54:23 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,14 +2,12 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-# Load biến môi trường từ file .env
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 DEFAULT_MODEL = "gemini-flash-latest"
 
 def get_genai_client() -> genai.Client:
-    """Khởi tạo và trả về Gemini Client."""
     if not GEMINI_API_KEY:
         raise ValueError("Không tìm thấy GEMINI_API_KEY. Vui lòng kiểm tra file .env!")
     return genai.Client(api_key=GEMINI_API_KEY)
